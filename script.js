d396972ddddc9a1cb405c89786bce6d8addf6042
Yago Araujo <yagoaraujogomes2@gmail.com>
2021-09-17 20:57:30 -0300

    movimentos

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x = 10 * box,
-    y = 10 * box
+    x : 8 * box,
+    y : 8 * box
 }
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 20 * box, 20 * box)
+    context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 function criarCobra() {
@@ -19,5 +20,23 @@ function criarCobra() {
     }
 }
 
-criarBG();
-criarCobra();
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    snake.pop();
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100)
